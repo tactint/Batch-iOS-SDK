1f8b24d80abe12173348ff06e795dac2effebbf4
Jud Saoud <68043755+judsd@users.noreply.github.com>
2024-04-18 01:20:58 +0300

    Added build3 to the url to avoid cache

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ONSBatch",
-            url: "https://ons.pfs.gdn/assets/96398dee-6d62-422e-9b78-0dcf0393420f/xcframework.zip",
+            url: "https://ons.pfs.gdn/assets/96398dee-6d62-422e-9b78-0dcf0393420f/xcframework-build3.zip",
             checksum: "232275d7c22edb00cbe6909cd2f5a13cdb89c0f63b5f4ac11f65a8db909f20b0"
         )
     ]
