0c561f68f08e3f51a3e42786b587465740bdbc95
Jud Saoud <68043755+judsd@users.noreply.github.com>
2024-04-18 01:10:12 +0300

    Update binaryTarget checksum

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
         .binaryTarget(
             name: "ONSBatch",
             url: "https://ons.pfs.gdn/assets/96398dee-6d62-422e-9b78-0dcf0393420f/xcframework.zip",
-            checksum: "b08b662e54e4d86cccbea490502345ad6f6f71479f6705e83301e674a4b3d6cd"
+            checksum: "232275d7c22edb00cbe6909cd2f5a13cdb89c0f63b5f4ac11f65a8db909f20b0"
         )
     ]
 )
