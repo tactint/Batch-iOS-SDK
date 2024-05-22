dc217891dd3e7d3375c862841bf12fb78a6ffbcc
Jud Saoud <68043755+judsd@users.noreply.github.com>
2024-05-22 09:30:10 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ONSBatch",
-            url: "https://ons.pfs.gdn/assets/96398dee-6d62-422e-9b78-0dcf0393420f/xcframework-build5.zip",
+            url: "https://ons.pfs.gdn/assets/a741b936-e743-4416-afa4-7494df66d54f/xcframework-build5.zip",
             checksum: "a2db092e556e325b9323c7d6a5f08fdd11e5e0b0712c20b9587055ab8c3e9c0d"
         )
     ]
